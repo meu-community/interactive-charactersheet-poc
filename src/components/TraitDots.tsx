2d36bfc3cbfdfda1c7d8fdc13b6e5b22bcfaed9c
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2024-11-24 20:01:31 -0500

    improve TraitDots rendering code

--- src/components/TraitDots.tsx
+++ src/components/TraitDots.tsx
@@ -6,20 +6,20 @@ export interface TraitDotsProps {
 }
 
 const TraitDots = ({ value, current }: TraitDotsProps) => {
-  // This should work but for some reason it isn't so for now we do it the procedural way.
-  // const dots = Array(value).map(() => (
-  //   <FontAwesomeIcon icon={["far", "circle"]} />
-  // ));
   const solidDotCount = current || 0;
   const emptyDotCount = value < solidDotCount ? 0 : value - solidDotCount;
-  const dots = [];
-  for (let i = 0; i <= solidDotCount - 1; i++) {
-    dots.push(<FontAwesomeIcon icon={["fas", "circle"]} />);
-  }
-  for (let i = 0; i <= emptyDotCount - 1; i++) {
-    dots.push(<FontAwesomeIcon icon={["far", "circle"]} />);
-  }
-  return <>{dots}</>;
+  const solidDots = Array(solidDotCount).fill(
+    <FontAwesomeIcon icon={["fas", "circle"]} />
+  );
+  const emptyDots = Array(emptyDotCount).fill(
+    <FontAwesomeIcon icon={["far", "circle"]} />
+  );
+  return (
+    <>
+      {solidDots}
+      {emptyDots}
+    </>
+  );
 };
 
 export default TraitDots;
